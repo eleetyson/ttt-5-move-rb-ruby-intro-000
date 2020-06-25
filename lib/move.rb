a94dc25b1c1d999807495741fddba306c7d0a2e5
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-06-25 23:57:16 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -19,4 +19,9 @@ def move(array, index, character)
   # update the index for where the user played
   array[index] = character
   # return the updated array (the board)
+  puts " #{array[0]} | #{array[1]} | #{array[2]} "
+  puts "-----------"
+  puts " #{array[3]} | #{array[4]} | #{array[5]} "
+  puts "-----------"
+  puts " #{array[6]} | #{array[7]} | #{array[8]} "
 end
